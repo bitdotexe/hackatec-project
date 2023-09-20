acc202630a49c3f0fb6122a5d45bc87f8d014d64
bit <uriel09890@gmail.com>
2023-09-20 13:17:30 -0600

    Creando app

---
+++ src/components/MainPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>Hola</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainPage",
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Quicksand&display=swap");
+
+* {
+  font-family: "Quicksand", sans-serif;
+}
+
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  display: flex;
+  height: 100%;
+}
+</style>
